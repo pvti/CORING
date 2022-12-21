Fix min_min, min_sum (row, col scope). Main now takes sort_idx pre-calculation

calculate_rank.py
@@ -1,9 +1,9 @@
 
 from tqdm.auto import tqdm
 import logging
+import copy
 import os
 import argparse
-from collections import OrderedDict
 import json
 import numpy as np
 from ranking_strategy import get_saliency
@@ -15,6 +15,12 @@ def get_parser():
     parser.add_argument('--input',
                         default='calculation/baseline/vgg/criteria.json',
                         help='path to load calculated correlation/norm file',
+                        )
+    parser.add_argument('--strategy',
+                        default=['sum', 'min_sum', 'min_min'],
+                        type=str,
+                        nargs='+',
+                        help='filter ranking strategy'
                         )
     parser.add_argument('--output',
                         default='calculation/baseline/vgg/rank.json',
@@ -38,7 +44,8 @@ if __name__ == "__main__":
                         )
     logging.info("Arguments: " + str(args))
 
-    with open(args.input, 'r') as file:
+    with open(args.input, 'r+') as file:
+        logging.info(f"=> loading '{args.input}'")
         data = json.load(file)
         dict = {
             "net": data["net"],
@@ -57,25 +64,29 @@ if __name__ == "__main__":
                 all_convs_sort_idx[i_conv] = sort_idx.tolist()
             sort_idx_dict[norm] = all_convs_sort_idx
 
-        # calculate saliency and rank for correlation
-        saliency_dict = {}
-        for criterion, all_convs_cor_mat in data["correlation"].items():
-            logging.info(f"-----{criterion}-----")
-            all_convs_saliency = {}
-            all_convs_sort_idx = {}
-            dis = 1 if 'dis' in criterion else -1
-            for i_conv, correl_matrix in tqdm(all_convs_cor_mat.items()):
-                mat = np.array(correl_matrix)
-                saliency = get_saliency(mat, dis)
-                sort_idx = saliency.argsort(kind='stable')[
-                    ::-1][:len(saliency)]
-                all_convs_saliency[i_conv] = saliency.tolist()
-                all_convs_sort_idx[i_conv] = sort_idx.tolist()
-            saliency_dict[criterion] = all_convs_saliency
-            sort_idx_dict[criterion] = all_convs_sort_idx
+        # calculate saliency and rank for correlation with strategy
+        for strategy in args.strategy:
+            logging.info(f"---------------{strategy}---------------")
+            saliency_dict = {}
+            for criterion, all_convs_cor_mat in data["correlation"].items():
+                logging.info(f"-----{criterion}-----")
+                all_convs_saliency = {}
+                all_convs_sort_idx = {}
+                dis = 1 if 'dis' in criterion else -1
+                for i_conv, correl_matrix in tqdm(all_convs_cor_mat.items()):
+                    mat = np.array(correl_matrix)
+                    saliency = get_saliency(mat, strategy, dis)
+                    sort_idx = saliency.argsort(kind='stable')[
+                        ::-1][:len(saliency)]
+                    all_convs_saliency[i_conv] = saliency.tolist()
+                    all_convs_sort_idx[i_conv] = sort_idx.tolist()
+                saliency_dict[criterion] = all_convs_saliency
+                sort_idx_dict[criterion] = all_convs_sort_idx
 
-        dict["saliency"] = saliency_dict
-        dict["sort_idx"] = sort_idx_dict
+            dict[strategy] = {
+                "saliency": saliency_dict,
+                "sort_idx": copy.deepcopy(sort_idx_dict) #sort_idx_dict CHANGED in loop
+                }
 
-        with open(args.output, 'w') as file:
+        with open(args.output, 'w+') as file:
             json.dump(dict, file, indent=4)

main.py
@@ -4,6 +4,7 @@ import os
 import argparse
 import numpy as np
 from collections import OrderedDict
+import json
 
 from data import *
 from helpers import *
@@ -39,6 +40,16 @@ def get_parser():
                         type=str,
                         nargs='+',
                         help='criteria'
+                        )
+    parser.add_argument('--strategy',
+                        default=['sum', 'min_sum', 'min_min'],
+                        type=str,
+                        nargs='+',
+                        help='filter ranking strategy'
+                        )
+    parser.add_argument('--calculated_rank',
+                        default='calculation/baseline/vgg/rank.json',
+                        help='path to load pre-calculated rank file',
                         )
     parser.add_argument('--output',
                         default='checkpoint/pruned/vgg/',
@@ -132,57 +143,71 @@ if __name__ == "__main__":
                                        )
     channel_pruning_ratios = np.around(channel_pruning_ratios, 2)
 
-    pruned_accuracy_dict = {c: [] for c in args.criteria}
-    finetuned_best_acc_dict = {c: [] for c in args.criteria}
     num_finetune_epochs = args.num_finetune_epochs
-    for criteria in args.criteria:
-        logging.info(f"---------------criteria--------------- {criteria}")
-        sorted_net = apply_channel_sorting(net, criteria)
-        for channel_pruning_ratio in channel_pruning_ratios:
-            logging.info(f"*******ratio******* {channel_pruning_ratio}")
-            pruned_net = channel_prune(sorted_net, channel_pruning_ratio)
-            pruned_net_accuracy = evaluate(pruned_net,
-                                           dataloader['test'],
-                                           criterion,
-                                           device
-                                           )
 
-            logging.info(f"pruned_net_acc = {pruned_net_accuracy:.2f}%")
-            pruned_accuracy_dict[criteria].append(
-                round((pruned_net_accuracy), 2))
+    # load pre-calculated sort idx
+    logging.info(f"=> loading '{args.calculated_rank}'")
+    f = open(args.calculated_rank, 'r')
+    data = json.loads(f.read())
 
-            # finetune then save
-            optimizer = torch.optim.SGD(pruned_net.parameters(),
-                                        lr=args.lr,
-                                        momentum=0.9,
-                                        weight_decay=1e-4
-                                        )
-            scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,
-                                                                   args.num_finetune_epochs)
+    pruned_acc_strategy = {}
+    finetuned_acc_strategy = {}
+    for strategy in args.strategy:
+        logging.info(f"------------------{strategy}------------------")
+        pruned_accuracy_dict = {c: [] for c in args.criteria}
+        finetuned_best_acc_dict = {c: [] for c in args.criteria}
+        for criteria in args.criteria:
+            logging.info(f"---------------{criteria}---------------")
+            sort_idx_dict = data[strategy]['sort_idx'][criteria]
+            sorted_net = apply_channel_sorting(net, sort_idx_dict)
+            for channel_pruning_ratio in tqdm(channel_pruning_ratios):
+                pruned_net = channel_prune(sorted_net, channel_pruning_ratio)
+                pruned_net_accuracy = evaluate(pruned_net,
+                                            dataloader['test'],
+                                            criterion,
+                                            device
+                                            )
 
-            finetuned_best_accuracy = 0
-            for epoch in range(start_epoch, start_epoch + num_finetune_epochs):
-                train(pruned_net, dataloader['train'],
-                      criterion, optimizer, scheduler, device)
-                acc = evaluate(pruned_net, dataloader['test'],
-                               criterion, device)
+                logging.info(f"pruned_net_acc = {pruned_net_accuracy:.2f}%")
+                pruned_accuracy_dict[criteria].append(
+                    round((pruned_net_accuracy), 2))
 
-                # save checkpoint if acc > best_acc
-                if acc > finetuned_best_accuracy:
-                    state = {'net': net.state_dict(),
-                             'acc': acc,
-                             'epoch': epoch,
-                             }
-                    path_save_net = os.path.join(args.output,
-                                                 f"{criteria}_{channel_pruning_ratio}.pth")
-                    torch.save(state, path_save_net)
-                    finetuned_best_accuracy = acc
+                # finetune then save
+                optimizer = torch.optim.SGD(pruned_net.parameters(),
+                                            lr=args.lr,
+                                            momentum=0.9,
+                                            weight_decay=1e-4
+                                            )
+                scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,
+                                                                    args.num_finetune_epochs)
 
-            finetuned_best_acc_dict[criteria].append(
-                round((finetuned_best_accuracy), 2))
+                finetuned_best_accuracy = 0
+                for epoch in range(start_epoch, start_epoch + num_finetune_epochs):
+                    train(pruned_net, dataloader['train'],
+                        criterion, optimizer, scheduler, device)
+                    acc = evaluate(pruned_net, dataloader['test'],
+                                criterion, device)
 
-            logging.info(get_model_performance(
-                pruned_net, dataloader, criterion))
+                    # save checkpoint if acc > best_acc
+                    if acc > finetuned_best_accuracy:
+                        state = {'net': net.state_dict(),
+                                'acc': acc,
+                                'epoch': epoch,
+                                }
+                        path_save_net = os.path.join(args.output,
+                                                    f"{criteria}_{channel_pruning_ratio}.pth")
+                        torch.save(state, path_save_net)
+                        finetuned_best_accuracy = acc
 
-    logging.info(pruned_accuracy_dict)
-    logging.info(finetuned_best_acc_dict)
+                finetuned_best_acc_dict[criteria].append(
+                    round((finetuned_best_accuracy), 2))
+
+                logging.info(get_model_performance(
+                    pruned_net, dataloader, criterion))
+
+        pruned_acc_strategy[strategy] = pruned_accuracy_dict
+        finetuned_acc_strategy[strategy] = finetuned_best_acc_dict
+    #logging.info(pruned_accuracy_dict)
+    #logging.info(finetuned_best_acc_dict)
+    logging.info(pruned_acc_strategy)
+    logging.info(finetuned_acc_strategy)

pruner.py
@@ -298,7 +298,7 @@ def get_saliency(correlation, dis=1):
 
 
 @torch.no_grad()
-def apply_channel_sorting(model, criteria):
+def apply_channel_sorting(model, sort_idx_dict):
     model = copy.deepcopy(model)  # do not modify the original model
     # fetch all the conv and bn layers from the backbone
     all_convs = [m for m in model.backbone if isinstance(m, nn.Conv2d)]
@@ -312,10 +312,8 @@ def apply_channel_sorting(model, criteria):
         prev_conv = all_convs[i_conv]
         prev_bn = all_bns[i_conv]
         next_conv = all_convs[i_conv + 1]
-        # note that we always compute the importance according to input channels
-        importance = get_input_channel_saliency(next_conv.weight, criteria)
-        # sorting from large to small
-        sort_idx = torch.argsort(importance, descending=True)
+
+        sort_idx = torch.tensor(sort_idx_dict[str(i_conv)], device = 'cuda')
 
         # apply to previous conv and its following bn
         prev_conv.weight.copy_(torch.index_select(