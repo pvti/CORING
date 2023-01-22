2ff27bfa617db8d5467502fadc7c6be46f54ce60
pvtien96 <pvtien96@gmail.com>
2023-01-22 08:57:38 +0100

    Fix scripts

--- main/scripts/resnet50_imagenet/vbd.sh
+++ main/scripts/resnet50_imagenet/vbd.sh
@@ -1,4 +1,4 @@
 for cpr in [0.]+[0.1]*3+[0.35]*16 [0.]+[0.12]*3+[0.38]*16 [0.]+[0.25]*3+[0.5]*16 [0.]+[0.5]*3+[0.6]*16
 do
-    python main/evaluate.py --data_dir data/imagenet/ --arch resnet_50 --job_dir result/resnet_50/vbd_dis --epochs 180 --lr_type cos --momentum 0.99 --pretrain_dir checkpoint/imagenet/resnet_50.pth --criterion VBD_dis --strategy min_sum --compress_rate $cpr --weight_decay 0.0001 --batch_size 256 --rank_conv_prefix rank --gpu 1
+    python main/evaluate.py --data_dir data/imagenet/ --arch resnet_50 --job_dir result/resnet_50/vbd_dis --epochs 180 --lr_type cos --momentum 0.99 --use_pretrain --pretrain_dir checkpoint/imagenet/resnet_50.pth --criterion VBD_dis --strategy min_sum --compress_rate $cpr --weight_decay 0.0001 --batch_size 256 --rank_conv_prefix rank
 done
--- main/scripts/vgg16_cifar10/kshot/cosine.sh
+++ main/scripts/vgg16_cifar10/kshot/cosine.sh
@@ -1,17 +1,17 @@
 #round 1
-for cpr in [0.21]*7+[0.75]*5 [0.3]*7+[0.75]*5 [0.45]*7+[0.78]*5
-do
-    python main/main_kshot.py --arch vgg_16_bn --job_dir kshot/round1 --use_pretrain --pretrain_dir checkpoint/cifar/cifar10/vgg_16_bn.pt --criterion cosine_sim --strategy min_sum --compress_rate $cpr --epochs 400 --weight_decay 0.005 --batch_size 256
-done
+#for cpr in [0.21]*7+[0.75]*5 [0.3]*7+[0.75]*5 [0.45]*7+[0.78]*5
+#do
+#    python main/main_kshot.py --arch vgg_16_bn --job_dir kshot/round1 --pretrain_dir checkpoint/cifar/cifar10/vgg_16_bn.pt --criterion cosine_sim --strategy min_sum --compress_rate $cpr --epochs 400 --weight_decay 0.005 --batch_size 256
+#done
 
 #round 2
 for cpr in [0.21]*7+[0.75]*5 [0.3]*7+[0.75]*5 [0.45]*7+[0.78]*5
 do
-    python main/main_kshot.py --arch vgg_16_bn --job_dir kshot/round2 --use_pretrain --pretrain_dir checkpoint/cifar/cifar10/vgg_16_bn.pt --criterion cosine_sim --strategy min_sum --compress_rate $cpr --epochs 400 --weight_decay 0.006 --batch_size 256
+    python main/main_kshot.py --arch vgg_16_bn --job_dir kshot/round2 --pretrain_dir checkpoint/cifar/cifar10/vgg_16_bn.pt --criterion cosine_sim --strategy min_sum --compress_rate $cpr --epochs 400 --weight_decay 0.006 --batch_size 256
 done
 
 #round 3
 for cpr in [0.21]*7+[0.75]*5 [0.3]*7+[0.75]*5 [0.45]*7+[0.78]*5
 do
-    python main/main_kshot.py --arch vgg_16_bn --job_dir kshot/round3 --use_pretrain --pretrain_dir checkpoint/cifar/cifar10/vgg_16_bn.pt --criterion cosine_sim --strategy min_sum --compress_rate $cpr --epochs 400 --weight_decay 0.005 --batch_size 128
+    python main/main_kshot.py --arch vgg_16_bn --job_dir kshot/round3 --pretrain_dir checkpoint/cifar/cifar10/vgg_16_bn.pt --criterion cosine_sim --strategy min_sum --compress_rate $cpr --epochs 400 --weight_decay 0.005 --batch_size 128
 done
