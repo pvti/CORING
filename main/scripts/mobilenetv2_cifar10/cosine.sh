5928ec4bf4f902c221c8d6fd4c7321dcda70db21
van-tien.pham <van-tien.pham@sms-cluster.lis-lab.fr>
2023-02-21 17:55:53 +0100

    Add pruning ratio for mobilenetv2 on CIFAR-10

---
+++ main/scripts/mobilenetv2_cifar10/cosine.sh
@@ -0,0 +1,17 @@
+#round 1
+for cpr in [0.]+[0.05]+[0.1]*2+[0.15]*2+[0.2]*2 [0.]+[0.1]+[0.25]*2+[0.25]*2+[0.3]*2
+do
+   python main/main.py --arch mobilenet_v2 --job_dir round1 --use_pretrain --pretrain_dir checkpoint/cifar/cifar10/mobilenet_v2.pt --criterion cosine_sim --strategy min_sum --compress_rate $cpr --epochs 400 --weight_decay 0.005 --batch_size 256
+done
+
+#round 2
+for cpr in [0.]+[0.05]+[0.1]*2+[0.15]*2+[0.2]*2 [0.]+[0.1]+[0.25]*2+[0.25]*2+[0.3]*2
+do
+   python main/main.py --arch mobilenet_v2 --job_dir round2 --use_pretrain --pretrain_dir checkpoint/cifar/cifar10/mobilenet_v2.pt --criterion cosine_sim --strategy min_sum --compress_rate $cpr --epochs 400 --weight_decay 0.006 --batch_size 256
+done
+
+#round 3
+for cpr in [0.]+[0.05]+[0.1]*2+[0.15]*2+[0.2]*2 [0.]+[0.1]+[0.25]*2+[0.25]*2+[0.3]*2
+do
+   python main/main.py --arch mobilenet_v2 --job_dir round3 --use_pretrain --pretrain_dir checkpoint/cifar/cifar10/mobilenet_v2.pt --criterion cosine_sim --strategy min_sum --compress_rate $cpr --epochs 400 --weight_decay 0.004 --batch_size 256
+done
