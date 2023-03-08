92094b27101e88c7c9e55824552c9ee994643a88
van-tien.pham <pvtien96@gmail.com>
2023-03-08 18:37:41 +0100

    Lightweight and extreme compression ratio for the teaser

--- main/scripts/vgg16_cifar10/kshot/3criteria.sh
+++ main/scripts/vgg16_cifar10/kshot/3criteria.sh
@@ -1,7 +1,7 @@
 #round 7
 for shot in 1 5 10 15
 do
-    for cpr in [0.21]*7+[0.75]*5 [0.3]*7+[0.75]*5 [0.45]*7+[0.78]*5
+    for cpr in [0.05]*7+[0.2]*5 [0.5]*2+[0.65]*5+[0.8]*5
     do
         python main/main_lr.py --arch vgg_16_bn --job_dir kshot/round7 --pretrain_dir checkpoint/cifar/cifar10/vgg_16_bn.pt --criterion cosine_sim --strategy min_sum --compress_rate $cpr --shot $shot --epochs 400 --lr_type cos --weight_decay 0.005 --batch_size 256
     done
@@ -9,7 +9,7 @@ done &
 
 for shot in 1 5 10 15
 do
-    for cpr in [0.21]*7+[0.75]*5 [0.3]*7+[0.75]*5 [0.45]*7+[0.78]*5
+    for cpr in [0.05]*7+[0.2]*5 [0.5]*2+[0.65]*5+[0.8]*5
     do
         python main/main_lr.py --arch vgg_16_bn --job_dir kshot/round7 --pretrain_dir checkpoint/cifar/cifar10/vgg_16_bn.pt --criterion Euclide_dis --strategy min_sum --compress_rate $cpr --shot $shot --epochs 400 --lr_type cos --weight_decay 0.005 --batch_size 256 --gpu 1
     done
@@ -17,7 +17,7 @@ done &
 
 for shot in 1 5 10 15
 do
-    for cpr in [0.21]*7+[0.75]*5 [0.3]*7+[0.75]*5 [0.45]*7+[0.78]*5
+    for cpr in [0.05]*7+[0.2]*5 [0.5]*2+[0.65]*5+[0.8]*5
     do
         python main/main_lr.py --arch vgg_16_bn --job_dir kshot/round7 --pretrain_dir checkpoint/cifar/cifar10/vgg_16_bn.pt --criterion VBD_dis --strategy min_sum --compress_rate $cpr --shot $shot --epochs 400 --lr_type cos --weight_decay 0.005 --batch_size 256 --gpu 2
     done
