d23e7d45d197c596d93bb7b5e210402d815d826b
pvtien96 <pvtien96@gmail.com>
2023-03-09 08:55:08 +0100

    Run VBD for Resnet50/ImageNet

--- main/scripts/resnet50_imagenet/vbd.sh
+++ main/scripts/resnet50_imagenet/vbd.sh
@@ -1,4 +1,4 @@
 for cpr in [0.]+[0.5]*3+[0.6]*16 [0.]+[0.1]*3+[0.35]*16 [0.]+[0.12]*3+[0.38]*16 [0.]+[0.25]*3+[0.5]*16
 do
-    CUDA_VISIBLE_DEVICES=0,1 python main/evaluate.py --data_dir data/imagenet/ --arch resnet_50 --job_dir round2 --epochs 200 --lr_type cos --momentum 0.99 --use_pretrain --pretrain_dir checkpoint/imagenet/resnet_50.pth --criterion VBD_dis --strategy min_sum --compress_rate $cpr --weight_decay 0.0001 --batch_size 256 --rank_conv_prefix rank --gpu '0 1'
+    python main/evaluate_disable_thop.py --data_dir data/imagenet/ --arch resnet_50 --job_dir round3 --epochs 200 --lr_type cos --momentum 0.99 --use_pretrain --pretrain_dir checkpoint/imagenet/resnet_50.pth --criterion VBD_dis --strategy min_sum --compress_rate $cpr --weight_decay 0.0001 --batch_size 512 --rank_conv_prefix rank --gpu 0,1,2
 done
