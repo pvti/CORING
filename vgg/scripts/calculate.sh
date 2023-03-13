c71dc49152996e6f71cfd237a530050ca9bd9da0
VanTien PHAM <pvtien96@gmail.com>
2023-03-13 08:54:40 +0100

    Remove vgg folder

--- vgg/scripts/calculate.sh
+++
@@ -1,9 +0,0 @@
-python -m calculation.calculate_svd --decomposer hosvd &&
-
-python -m calculation.calculate_correlation_svd --correlation cosine_sim Euclide_dis VBD_dis &&
-
-python -m calculation.calculate_correlation --correlation cosine_sim Euclide_dis VBD_dis &&
-
-python -m calculation.merge_correlation &&
-
-python -m calculation.calculate_rank
--- vgg/scripts/train_baseline.sh
+++
@@ -1,3 +0,0 @@
-python train.py --arch VGG16 --output checkpoint/baseline/vgg/vgg16.pth --num_epochs 500 --wandb_name vgg16_train &&
-
-python train.py --arch Resnet18 --output checkpoint/baseline/resnet/resnet18.pth --num_epochs 500 --wandb_name resnet18_train
