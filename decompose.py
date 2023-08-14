bb67022370f7cec076201dc0c18abd4937de865b
tien <pvtien96@gmail.com>
2023-08-14 18:20:30 +0200

    Add tensorly's tucker as a decomposer

--- decompose.py
+++ decompose.py
@@ -1,30 +1,36 @@
 import torch
 import tensorly as tl
 from tensorly import unfold
+from tensorly.decomposition import tucker
 
-tl.set_backend('pytorch')
+
+tl.set_backend("pytorch")
 
 
 def get_num_unfold(decomposer):
-    """Get number of unfolding based on decomposer
-    """
+    """Get number of unfolding based on decomposer"""
 
-    return 1 if decomposer == 'svd' else 3
+    return 1 if decomposer == "svd" else 3
 
 
 def get_u_svd(x):
-    """Perform SVD, return 1st column of u
-    """
+    """Perform SVD, return 1st column of u"""
     u, _, _ = torch.linalg.svd(x, full_matrices=False)
 
     return u[:, 0]
 
 
-def decompose(x, decomposer='hosvd'):
+def decompose(x, decomposer="hosvd"):
     """Get left_singulars based on decomposer
-        svd: u
-        hosvd: [u1, u2, u3]
+    svd: u
+    hosvd: [u1, u2, u3]
+    tucker: [u1, u2, u3]
     """
+    if decomposer == "tucker":
+        _, factors = tucker(x, rank=[1, 1, 1])
+
+        return factors
+
     num_unfold = get_num_unfold(decomposer)
     left_singulars = []
     for i in range(num_unfold):
